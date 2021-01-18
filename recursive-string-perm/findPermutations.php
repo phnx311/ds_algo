c5e49503867239e9f984a01f667606ec67872cbe
Eric Chi <echi8112@gmail.com>
2021-01-18 16:59:04 -0500

    begin php solution

--- recursive-string-perm/findPermutations.php
+++ recursive-string-perm/findPermutations.php
@@ -1,3 +1,15 @@
- <!-- Write a recursive function for generating all 
- permutations of an input string. 
- Return them as a set (use Ds library that come with PHP 7+ -->
+<? 
+
+use Ds\Set;
+// Write a recursive function for generating all 
+// permutations of an input string. 
+// Return them as a set (use Ds library that come with PHP 7+)
+
+function findPerms($string) {
+
+    //base case
+    if (strlen($string) <= 1) {
+        return new Set([$string]);
+    }
+
+}
