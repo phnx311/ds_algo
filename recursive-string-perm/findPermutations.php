73211d331b8561991a50e501eb8e7acda6abd20f
Eric Chi <ericchi.311@gmail.com>
2021-01-19 14:34:01 -0500

    have php test case for findPerms

--- recursive-string-perm/findPermutations.php
+++ recursive-string-perm/findPermutations.php
@@ -1,30 +1,34 @@
-<? 
+<?php 
 
-use Ds\Set;
 // Write a recursive function for generating all 
 // permutations of an input string. 
-// Return them as a set (use Ds library that come with PHP 7+)
 
 function findPerms($string) {
 
     //base case
     if (strlen($string) <= 1) {
-        return new Set([$string]);
+        return [$string];
     }
     
     $allCharsExceptLast = substr($string, 0, - 1);
-    $lastCharacter = $string[strnlen($string) - 1];
+    $lastCharacter = $string[strlen($string) - 1];
 
     $permutationsOfAllCharsExceptLast = findPerms($allCharsExceptLast);
 
-    $permutations = new Set();
+    $permutations = [];
 
-    foreach($permuationsOfAllCharsExceptLast as $permutationOfAllCharsExceptLast) {
-        for ($position = 0; $position <= strnlen($allCharsExceptLast); $position++) {
+    foreach($permutationsOfAllCharsExceptLast as $permutationOfAllCharsExceptLast) {
+        for ($position = 0; $position <= strlen($allCharsExceptLast); $position++) {
             $permutation = substr($permutationOfAllCharsExceptLast, 0, $position) . $lastCharacter . substr($permutationOfAllCharsExceptLast, $position);
-            $permutations->add($permutation);
+            array_push($permutations, $permutation);
         }
     }
 
     return $permutations;
 }
+
+$result = findPerms("cat");
+
+var_dump($result);
+
+?>
