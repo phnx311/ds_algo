1f96f76bbfb91de81120c5a5122ba7a43c4d6129
Eric Chi <ericchi.311@gmail.com>
2021-01-28 06:31:50 -0500

    php solution for reverse linked list in place

--- linked-lists/reverse-linked-list/reverseLinkedList.php
+++ linked-lists/reverse-linked-list/reverseLinkedList.php
@@ -1,6 +1,55 @@
 <?php
+class LinkedListNode 
+{
+    private $value;
+    private $next;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getNext() 
+    {
+        return $this->next;
+    }
+
+    public function setNext($next) 
+    {
+        $this->next = $next;
+    }
+
+    public function setValue($value) 
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
 /**
  * @param LinkedListNode $linkedListHead 
  * @return LinkedListNode 
  */
+function reverse($linkedListHead) 
+{
+    $currentNode = $linkedListHead;
+    $prevNode = null;
+    $nextNode = null;
+
+    while($currentNode) 
+    {
+        $nextNode = $currentNode->getNext();
+
+        $currentNode->setNext($prevNode);
+
+        $prevNode = $currentNode;
+        $currentNode = $nextNode;
+    }
+
+    return $prevNode;
+}
 ?>
